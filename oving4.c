ca0c8453dc3b8026400c3a0e043a0b3b8edcf19f
Torje Hoås Digernes <torjehoa@pvv.org>
2012-02-06 15:59:18 +0100

    laget omp loop

--- oving4.c
+++ oving4.c
@@ -17,17 +17,23 @@ double sum_of_inf(){
 	return pow(M_PI, 2)/6;
 }
 
-double sum_vector ( double * vec , long n ){
+double sum_vector ( double * vec , long n )
+{
 	double * limit = vec + n , out = 0;
 
 	double * ptr = vec, * ptr2 = vec; 
+	//Some sanity checks 
 	if ( n == 1 ) {
-		return *vec;
+		return vec[0];
 	}
+	else if ( n==0){
+		return 0;
+	}
+	
+	// Sum the contents of the vector and store the sum in the beginning of the array. The loop will stop at the second last element should it be odd membered, the if afterwards will handle that case;
 	for ( ; ptr+1 < limit; ptr+=2, ptr2++ ){
 		*ptr2= ptr[0] + ptr[1];
 	}
-
 	if ( ptr < limit ) {
 		ptr2[0]= ptr[0];
 	}
@@ -39,6 +45,27 @@ double sum_vector ( double * vec , long n ){
 		return sum_vector( vec, ( n / 2 ) + ( n & 1 ) );
 	}
 	return  out;
+}
+
+double sum_vector_openMP(double * vec, long n )
+{
+	double * limit = vec + n , sum = 0;
+
+	double * ptr = vec, * ptr2 = vec; 
+	//Some sanity checks 
+	if ( n == 1 ) {
+		return vec[0];
+	}
+	else if ( n==0){
+		return 0.0;
+	}
+	
+	// Sum the contents of the vector and store the sum in the beginning of the array. The loop will stop at the second last element should it be odd membered, the if afterwards will handle that case;
+#pragma omp paralell for reduction( + : sum )
+	for ( ; ptr < limit; ++ptr ){
+		sum= ptr[0];
+	}
+	return sum;
 }
 
 int main (int argc , char ** argv){
