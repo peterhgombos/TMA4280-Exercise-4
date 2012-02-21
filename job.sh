b4349a9423a185d7e05a616fdd8736b05509fca2
Torje Hoas Digernes <torjehoa@kongull.hpc.ntnu.no>
2012-02-21 16:54:01 +0100

    job.sh lagt til

--- job.sh
+++ job.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #PBS -q optimist
-#PBS -lnodes=1:ppn=12
+#PBS -lnodes=2:ppn=12
 #PBS -lwalltime=00:00:30
 #PBS -V
 #PBS -lpmem=2MB
+#PBS -A freecycle
 
 cd $PBS_O_WORKDIR
-mpirun -np 12 TMA4280-Exercise-4/a.out
+mpirun -np 12 a.out > output
  
 exit 0
